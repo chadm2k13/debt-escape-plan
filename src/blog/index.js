16102d01785f92a6bf316ea8cd15e2457596e8c7
chadm2k13 <chadm2k13@gmail.com>
2026-04-17 19:50:16 -0400

    Update index.js

--- src/blog/index.js
+++ src/blog/index.js
@@ -1,4 +1,7 @@
 import { meta as post1 } from "./posts/personalized-bridal-shower-game.jsx";
 import { meta as post2 } from "./posts/how-well-do-you-know-the-bride-questions.jsx";
+import { meta as post3 } from "./posts/best-bridal-shower-games-large-groups.jsx";
+import { meta as post4 } from "./posts/baby-shower-games-everyone-will-love.jsx";
+import { meta as post5 } from "./posts/bridal-shower-on-a-budget.jsx";
 
-export const allPosts = [post2, post1];
+export const allPosts = [post5, post4, post3, post2, post1];
