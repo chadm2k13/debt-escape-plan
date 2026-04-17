6beb9f31915e4835924f7c94f9f0139d03789ec2
chadm2k13 <chadm2k13@gmail.com>
2026-04-17 19:17:22 -0400

    Create index.js

---
+++ src/blog/index.js
@@ -0,0 +1,4 @@
+import { meta as post1 } from "./posts/personalized-bridal-shower-game.jsx";
+import { meta as post2 } from "./posts/how-well-do-you-know-the-bride-questions.jsx";
+
+export const allPosts = [post2, post1];
