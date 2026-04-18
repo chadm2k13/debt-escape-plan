ce98596ed08e4865e28907401ae128d5161b0337
chadm2k13 <chadm2k13@gmail.com>
2026-04-17 20:26:12 -0400

    Update index.js

--- src/blog/index.js
+++ src/blog/index.js
@@ -3,5 +3,8 @@ import { meta as post2 } from "./posts/how-well-do-you-know-the-bride-questions.
 import { meta as post3 } from "./posts/best-bridal-shower-games-large-groups.jsx";
 import { meta as post4 } from "./posts/baby-shower-games-everyone-will-love.jsx";
 import { meta as post5 } from "./posts/bridal-shower-on-a-budget.jsx";
+import { meta as post6 } from "./posts/baby-shower-trivia-questions.jsx";
+import { meta as post7 } from "./posts/bachelorette-party-games-not-cringe.jsx";
+import { meta as post8 } from "./posts/bridal-shower-games-small-groups.jsx";
 
-export const allPosts = [post5, post4, post3, post2, post1];
+export const allPosts = [post8, post7, post6, post5, post4, post3, post2, post1];
